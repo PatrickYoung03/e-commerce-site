01da0a4f6dfe6839320a256b484157dc58b8c854
PatrickYoung03 <patrickyoung18@googlemail.com>
2021-01-01 16:16:05 +0000

    Refactored to MVC

--- app.js
+++ app.js
@@ -3,35 +3,24 @@ const PORT = 3000;
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const app = express();
 
-// HANDLEBARS
-// app.engine(
-//   "hbs",
-//   expressHbs({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
+const errorController = require("./controllers/error");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.status(404).render("error", { pageTitle: "Page not found" });
-});
+app.use("/", errorController.get404);
 
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
